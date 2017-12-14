56c084039e5aaa7f5763a577dd0e465e1a7858aa
tbeckham <tony.beckham@gmail.com>
2017-12-14 09:04:43 -0800

    Lambdafy upload portfolio script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,20 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.tonybeckham.com')
-build_bucket = s3.Bucket('portfoliobuild.tonybeckham.com')
+    portfolio_bucket = s3.Bucket('portfolio.tonybeckham.com')
+    build_bucket = s3.Bucket('portfoliobuild.tonybeckham.com')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
+    with zipfile.ZipFile(portfolio_zip) as myzip:
       for nm in myzip.namelist():
          obj = myzip.open(nm)
          portfolio_bucket.upload_fileobj(obj, nm,
            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
          portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print "Job Done!"
+    return 'Hello from Lambda'
