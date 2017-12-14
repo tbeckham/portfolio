3eab07db7037e11cf73bcfe980aecba1d2fb282d
tbeckham <tony.beckham@gmail.com>
2017-12-14 10:52:51 -0800

    Change region for resources in  upload portfolio script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,13 +6,12 @@ import mimetypes
 
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-west-2:779646646979:deployPortfolioTopic')
-
+    topic = sns.Topic('arn:aws:sns:us-east-1:779646646979:deployPortfolioTopic')
     try:
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket('portfolio.tonybeckham.com')
-        build_bucket = s3.Bucket('portfoliobuild.tonybeckham.cm')
+        build_bucket = s3.Bucket('portfoliobuild.tonybeckham.com')
 
         portfolio_zip = StringIO.StringIO()
         build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
@@ -23,11 +22,9 @@ def lambda_handler(event, context):
              portfolio_bucket.upload_fileobj(obj, nm,
                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
              portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
         print "Job Done!"
         topic.publish(Subject = "Portfolio Deployed", Message="Portfolio deployed successfully!")
     except:
-         topic.publish(Subject = "Portfolio Deploy Failed", Message="Portfolio was not deployed.")
-         raise
-
+        topic.publish(Subject = "Portfolio Deploy Failed", Message="Portfolio was not deployed.")
+        raise
     return 'Hello from Lambda'
