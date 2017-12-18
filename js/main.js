6c94a11fb897f20c6e5ba4bfc94083120b85e13b
tbeckham <tony.beckham@gmail.com>
2017-12-18 11:29:20 -0800

    Move static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log('Loaded react-dom');
+const myWork = [
+  {
+    'title': "Portfolio",
+    'image': {
+      'desc': "A serverless portfolio",
+      'src': "images/3wolfmoon-download.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Wildrydes",
+    'image': {
+      'desc': "A serverless Webapp",
+      'src': "images/wildrydes-homepage.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "eucalyptus-cookbook",
+    'image': {
+      'desc': "Chef cookbook for deploying and configuring Eucalyptus",
+      'src': "images/chef-logo.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "awesomeProject",
+    'image': {
+      'desc': "Messing with zeromq",
+      'src': "images/zmq-octogon.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "N4j",
+    'image': {
+      'desc': "An AWS java test framework",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
